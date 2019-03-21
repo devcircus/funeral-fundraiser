20fc03a6397a1457d40421a9db3fdb06df1fe670
Clayton Stone <devcircus@outlook.com>
2019-03-21 11:40:36 -0500

    Remove donation form.

--- resources/views/donations/create.blade.php
+++ resources/views/donations/create.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
 <div class="w-full mx-auto -mt-6">
-    <h1 class="text-grey-darkest text-2xl">Thanks for visiting.</h1><br>
-    <h2 class="text-grey-dark text-xl mb-8">Fill the form below to make your donation.</h2>
-    <stripe-component></stripe-component>
+    <h1 class="text-grey-darkest text-2xl">Thank you to everyone for your donations. We couldn't have done it without you!</h1><br>
+    <!-- <h2 class="text-grey-dark text-xl mb-8">Fill the form below to make your donation.</h2> -->
+    <!-- <stripe-component></stripe-component> -->
 </div>
 @stop
